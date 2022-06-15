9cadec5df7b3a50a2924438341022a579b643222
Cesare Ferrari <cesar89@live.it>
2022-06-15 15:35:18 +0200

    test 2 commit git

--- src/App.js
+++ src/App.js
@@ -4,10 +4,6 @@ import SearchBox from "./components/search-box/search-box.component";
 import logo from "./logo.svg";
 import "./App.css";
 
-//first change to push in a branch
-
-
-
 //esempio di componente funzionale
 // function App() {
 //   return (
