ff18c50eedb43ea7f532dc432ccbde3e8a7a5119
Cesare Ferrari <cesar89@live.it>
2022-06-15 19:24:08 +0200

    hooks

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import { useState, useEffect } from "react"; //hook
+import { useState, useEffect } from "react"; 
 import CardList from "./components/card-list/card-list.component";
 import SearchBox from "./components/search-box/search-box.component";
 import "./App.css";
 
 const App = () => {
 
-  const [searchField, setSearchField] = useState(""); // [value, setValue]
+  const [searchField, setSearchField] = useState(""); 
   const [monsters, setMonsters] = useState([]);
   const [filteredMonsters, setFilteredMonsters] = useState(monsters);
 
